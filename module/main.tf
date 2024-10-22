2868c50815c6d19996af36af607f6a2287937f38
Andrii Penchuk <penja7@gmail.com>
2024-10-22 13:34:51 +0300

    test without providers

--- module/main.tf
+++ module/main.tf
@@ -6,9 +6,19 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
+#resource "aws_s3_bucket" "test_bucket" {
+#  bucket = var.bucket_name
+#  acl    = "private"
+#}
+
+
+resource "null_resource" "test" {
+    provisioner "local-exec" {
+        command = "echo 'Hello, World!'"
+    }
+    triggers = {
+        always_run = timestamp()
+    }
 }
 
 output "bucket_name" {
