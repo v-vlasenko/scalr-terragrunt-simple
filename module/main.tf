33522def9056ab946287d89056a4fb2e12a076f5
Andrii Penchuk <penja7@gmail.com>
2024-10-22 13:37:21 +0300

    fix

--- module/main.tf
+++ module/main.tf
@@ -21,11 +21,11 @@ resource "null_resource" "test" {
     }
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.test_bucket.bucket
-}
+#output "bucket_name" {
+#  value = aws_s3_bucket.test_bucket.bucket
+#}
 
 
-output "bucket_arn" {
-  value = aws_s3_bucket.test_bucket.arn
-}
+#output "bucket_arn" {
+#  value = aws_s3_bucket.test_bucket.arn
+#}
