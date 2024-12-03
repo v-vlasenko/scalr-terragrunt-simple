3bf342611c5a40ecd2f046f9cfea5635eee225b9
Andrii Penchuk <penja7@gmail.com>
2024-12-03 16:53:15 +0100

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -13,6 +13,11 @@ provider "aws" {
 #  acl    = "private"
 # }
 
+module "one" {
+	source  = "penja.scalr.io/env-v0ojkc698lr47ibhs/one/double"
+	version = "1.0.3"
+}
+
 
 resource "null_resource" "test" {
   provisioner "local-exec" {
